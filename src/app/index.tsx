d735bf771c1324ac6948fdd499244a60ad51295c
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-05-16 20:25:58 +0100

    Header abstraction

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,33 +1,18 @@
-import { StyleSheet, Text, View } from "react-native";
-
+import HomeHeader from "@/components/HomeHeader";
+import { globalStyles } from "@/styles/global";
+import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>CybKit</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
-    </View>
+    <ScrollView style={globalStyles.container}>
+      <Text style={globalStyles.title}>CybKit</Text>
+      <HomeHeader />
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#003262',
-    paddingTop: 60,
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: 'bold',
-    color: '#ffffff',
-  },
-  date: {
-    fontSize: 14,
-    color: '#a0a0b0',
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
+
 
 
