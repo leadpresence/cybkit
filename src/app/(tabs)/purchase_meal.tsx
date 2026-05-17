e0368937d0c166bfce44b869daf87bff6c1549bf
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-05-17 08:35:58 +0100

    Added meal purchase screen

---
+++ src/app/(tabs)/purchase_meal.tsx
@@ -0,0 +1,10 @@
+import { globalStyles }  from "@/styles/global";
+import { Text,View } from "react-native";
+
+export default function PurchaseMealScreen() {
+    return (
+        <View style={globalStyles.container}>
+            <Text style={globalStyles.title}>Purchase Meal</Text>
+        </View>
+    )
+}
