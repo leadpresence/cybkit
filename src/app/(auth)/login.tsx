fd05978321c356c03bbccd2e238126a608fdf28e
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-05-17 16:59:30 +0100

    Implement login screen with staff authentication

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,13 +1,49 @@
 import { LoginHeader } from "@/components/HomeHeader";
-import { globalStyles } from "@/styles/global";
-import { ScrollView, Text } from "react-native";
+import { colors, globalStyles } from "@/styles/global";
+import { useState } from "react";
+import {
+    ScrollView,
+    Text,
+    TextInput,
+    TouchableOpacity
+} from "react-native";
 
 export default function LoginScreen() {
+      const [staffId, setStaffId] = useState('');
+      const [password, setPassword] = useState('');
+
+  const handleAddMeal = () => {
+    console.log({ staffId, password });
+  };
     return (
         <ScrollView>
          <Text style={globalStyles.title}>Login</Text>
             
             <LoginHeader />
+
+               <TextInput
+                    style={globalStyles.input}
+                    placeholder='Staff ID'
+                    placeholderTextColor={colors.textSecondary}
+                    value={staffId}
+                    onChangeText={setStaffId}
+                  />
+            
+                  <TextInput
+                    style={globalStyles.input}
+                    placeholder='Password'
+                    placeholderTextColor={colors.textSecondary}
+                    secureTextEntry
+                    value={password}
+                    onChangeText={setPassword}
+                  />
+
+
+                        <TouchableOpacity style={globalStyles.button} onPress={handleAddMeal}>
+                          <Text style={globalStyles.buttonText}>Add Meal</Text>
+                        </TouchableOpacity>
         </ScrollView>
     )
 }
+
+ 
