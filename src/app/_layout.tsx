41f083b2db8c7c480e08a25240519dc9f5fadde0
Chibueze  felix <33674336+leadpresence@users.noreply.github.com>
2026-05-17 08:36:23 +0100

    Fix:Modified the tabs and auth layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,11 +2,9 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-      <Stack
-      screenOptions={{
-        headerShown: false,
-
-      }}
-    />
+      <Stack screenOptions={{ headerShown: false }} >
+    <Stack.Screen name='(auth)'  />
+    <Stack.Screen name='(tabs)'  />
+    </Stack>
   );
 }
